7d5a59362f7586091f3993e72988aab1839ac67e
Sachin Patney <sachin.patney@gmail.com>
2016-11-26 14:43:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ var notify = require('./notify')();
 var garage = require('./garage.js')(notify);
 var catToy = require('./cat.js')();
 //var thermp = require('./thermo.js')(); // enable when you have one
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.post('/register', function(req, res) {
     var token = req.body['token'];
     notify.register(token);
-    res.json({'result': 'ok'});
+    res.json({'result': 'ok'});app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 });
 
 app.post('/device', function(req, res){
